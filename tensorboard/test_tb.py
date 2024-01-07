2502419f98d3f59a04e89a2f7d358320cd017134
yzh92 <2426555967@qq.com>
2024-01-07 15:21:30 +0800

    using tensorboard writer.add_image()

--- tensorboard/test_tb.py
+++ tensorboard/test_tb.py
@@ -1,15 +1,28 @@
 from torch.utils.tensorboard import SummaryWriter
-
+import numpy as np
+from PIL import Image
 writer = SummaryWriter("logs")
 
-# writer.add_image()
+
 # writer.add_scalar()
+
 # y = x
 # for i in range(100):
 #     writer.add_scalar("y=x", i, i)
+
 # y = 2x
-for i in range(100):
-    writer.add_scalar("y=2x", 2*i, i)
+# for i in range(100):
+#     writer.add_scalar("y=2x", 2*i, i)
+# writer.close()
+
+# writer.add_image()
+image_path = 'D:\Python project\pytorch_learning\dataset_other_show\\train\\ants_image\\5650366_e22b7e1065.jpg'
+img_PIL = Image.open(image_path)
+print(type(img_PIL))
+img_array = np.array(img_PIL)
+print(type(img_array))
+writer.add_image('test',img_array,2,dataformats='HWC')
+
 writer.close()
 
 '''
