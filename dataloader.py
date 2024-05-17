069a69c6ace7d204f116cda000a6ce6cfb5b8ef1
yzh92 <2426555967@qq.com>
2024-05-17 16:23:25 +0800

    2024_5_17

---
+++ dataloader.py
@@ -0,0 +1,21 @@
+import torchvision
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+# test_data
+test_data = torchvision.datasets.CIFAR10("./datasets_tranforms/CIFAR10",download=True,train=False,transform=torchvision.transforms.ToTensor())
+
+test_loader = DataLoader(dataset=test_data,batch_size=64,shuffle=True,num_workers=0,drop_last=True)
+# the first image and target in test_data
+img,target = test_data[0]
+print(img.shape)
+print(target)
+
+writer = SummaryWriter("dataloader")
+step = 0
+for data in test_loader:
+    imgs,targets = data
+    print(imgs.shape)
+    print(targets)
+    writer.add_images('test_data_drop_last',imgs,step)
+    step += 1
--- datasets_tranforms/transforms_use_datasets.py
+++ datasets_tranforms/transforms_use_datasets.py
@@ -1,4 +1,13 @@
 import torchvision
+dataset_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor])
 
 train_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=True,download=True)
 test_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=False,download=True)
+
+print(train_set[0])
+print(train_set.classes)
+img,target = train_set[0]
+print(img)
+print(target)
+print(train_set.classes[target])
+img.show()
