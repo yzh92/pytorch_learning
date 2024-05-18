484e0814837bd4cd8114cd438099470c3b288c68
yzh92 <2426555967@qq.com>
2024-05-18 16:41:24 +0800

    transforms_use

--- datasets_tranforms/transforms_use_datasets.py
+++ datasets_tranforms/transforms_use_datasets.py
@@ -1,13 +1,22 @@
 import torchvision
-dataset_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor])
+from torch.utils.tensorboard import SummaryWriter
 
-train_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=True,download=True)
-test_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=False,download=True)
+dataset_transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
 
-print(train_set[0])
-print(train_set.classes)
-img,target = train_set[0]
-print(img)
-print(target)
-print(train_set.classes[target])
-img.show()
+train_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=True,transform=dataset_transform,download=True)
+test_set = torchvision.datasets.CIFAR10(root='./CIFAR10',train=False,transform=dataset_transform,download=True)
+# print(test_set[0])
+# print(train_set[0])
+# print(train_set.classes)
+# img,target = train_set[0]
+# print(img)
+# print(target)
+# print(train_set.classes[target])
+# img.show()
+
+writer = SummaryWriter('p10')
+for i in range(10):
+    img,target = test_set[i]
+    writer.add_image('test_set',img,i)
+
+writer.close()
